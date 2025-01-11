0a6076fed6bc3ab953605ee751e041894c17cbfc
Amit Gangwar <gangwaramit449@gmail.com>
2025-01-11 10:15:43 +0530

    Explained Classes and Objects

---
+++ OOPs - Again/module1.cpp
@@ -0,0 +1,39 @@
+/*
+ * Objects are entities in real world
+ * Classes is Like bueprint for these entities
+ * eg : Topyota created a blueprint for making cars -> using that they can create as many cars they want.
+ * Cars -> entities , Blueprint -> classes (group of objects)
+
+ * Teacher (name,dept.,subject,salary , [change dept]) (properties , Methods(funtions written inside the class))
+
+
+Class:
+Think of a class as a blueprint or template.
+It defines the structure or design of something but doesn’t exist on its own.
+It contains attributes (data) and behaviors (methods/functions).
+Example: Imagine a blueprint of a house. It describes:
+
+How many rooms the house will have.
+The size of the rooms.
+Where the doors and windows will be.
+But it’s not an actual house yet.
+--------------------------------------------------------------
+Object:
+An object is a specific, real-world instance of a class.
+It is created based on the blueprint (class) and has all the properties and behaviors defined by that blueprint.
+Example: If a class is a blueprint of a house, an object is an actual house built using that blueprint. You can have multiple houses (objects) from the same blueprint (class).
+
+ */
+
+/*
+ * OOP allows encapsulation, meaning data (attributes) and methods (functions) are bundled together in a class. 
+ * OOP allows you to represent real-world entities (like cars, animals, or students) in your code.
+ * OOP promotes code reuse through inheritance, where one class can inherit properties and behaviors of another class.
+ * OOP organizes code into classes, making it modular. This makes it easier to:
+     -> Debug
+     -> Update
+     -> Extend functionality
+     -> Why it matters: Changes in one part of the code don't affect other parts, reducing the risk of breaking the system.
+ * In OOP, different developers can work on different objects or classes independently.
+
+ */
