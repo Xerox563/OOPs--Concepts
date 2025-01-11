f685f062b8da18e45eca65aad621ff14b382cf1f
Amit Gangwar <gangwaramit449@gmail.com>
2025-01-11 11:03:49 +0530

    Completed Module 2 and Started Module 3

--- OOPs - Again/module2.cpp
+++ OOPs - Again/module2.cpp
@@ -14,6 +14,26 @@ private:
     string password;
 
 public:
+    Account()
+    {
+        cout << "HI !! , I am Constructor" << endl;
+    }
     string username;
 };
 
+int main()
+{
+    Account a1;
+    return 0;
+}
+
+/*
+-> Constructor -> Special Method invoked automatically at the time of Object creation -> Used For Initialization of Object.
+ * Same Name as Class Name.
+ * Do not have a return type. (always declared Public).
+ * Only called once (during Object Creation)
+ * Memory Allocation Happens When Constructor is Called .
+ * Memory Allocation Occurs for Objects not for classes (Only when the Object is Created and Constructor is called (automatically)).
+ * One class can have multiple constructors (But there parameters should be different .) -> Means Different constructors of different types -> Constructor Overloading (eg. of Polymorphism)
+ * this -> prop : *(this).prop : obj.prop
+ */
